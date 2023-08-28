7c9521bf795695b943d7ef659416285061055a3c
j-jeongeun <jejeong88@gmail.com>
2023-08-28 22:41:47 +0900

    기능수정: 미사용코드 삭제

--- src/main/java/project/singasong/oauth/naver/dto/NaverOauthDto.java
+++ src/main/java/project/singasong/oauth/naver/dto/NaverOauthDto.java
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 public class NaverOauthDto {
 
     private String access_token;
-    private String accessToken;
     private String refresh_token;
     private String token_type;
     private Long expires_in;
