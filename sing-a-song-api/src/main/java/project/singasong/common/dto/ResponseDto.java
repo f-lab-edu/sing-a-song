e2498c62d9b192c01834aaf8e870981d289288d9
j-jeongeun <jejeong88@gmail.com>
2023-10-15 21:25:29 +0900

    기능수정: 좋아요 기능 수정

---
+++ sing-a-song-api/src/main/java/project/singasong/common/dto/ResponseDto.java
@@ -0,0 +1,19 @@
+package project.singasong.common.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.singasong.common.enums.ResultMessage;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResponseDto<T> {
+
+    private ResultMessage message;
+    private T result;
+
+}
--- sing-a-song-api/src/main/java/project/singasong/like/controller/LikeController.java
+++ sing-a-song-api/src/main/java/project/singasong/like/controller/LikeController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import project.singasong.common.dto.ResponseDto;
+import project.singasong.common.enums.ResultMessage;
 import project.singasong.like.domain.Like;
 import project.singasong.like.dto.LikePagingDto;
 import project.singasong.like.service.LikeService;
@@ -39,7 +41,11 @@ public class LikeController {
             .songId(songId)
             .build();
 
-        return ResponseEntity.ok().body(likeService.like(like));
+        ResponseDto responseDto = ResponseDto.builder()
+            .message(ResultMessage.SUCCESS)
+            .result(likeService.like(like))
+            .build();
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @DeleteMapping("/like/{userId}/{songId}")
